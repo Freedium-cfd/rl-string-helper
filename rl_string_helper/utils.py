9305da3ad8657ef24a8dc3ba8868ceb5cd11eaa3
ZhymabekRoman <robanokssamit@yandex.ru>
2023-09-12 18:50:01 +0600

    CODE: fixing line break in code

--- rl_string_helper/utils.py
+++ rl_string_helper/utils.py
@@ -15,7 +15,7 @@ EXTRA_QUOTE_REPLACE_WITH = {"\n": "<br />", "\t": "&emsp;"}  # "  ": " &nbsp;"
 
 # https://stackoverflow.com/questions/1061697/whats-the-easiest-way-to-escape-html-in-python
 # XXX: disabling extra quoting as workaround
-def quote_html(html: str, extra: bool = True) -> str:
+def quote_html(html: str, extra: bool = False) -> str:
     for m in QUOTE_PATTERN.finditer(html):
         yield m.span(), QUOTE_REPLACE_WITH[m.group(1)]
     if extra:
